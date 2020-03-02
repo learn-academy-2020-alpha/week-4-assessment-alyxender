97758205c117b131ad12f1840cd25595025d437e
Ubuntu <ubuntu@ip-172-31-95-79.ec2.internal>
2020-03-02 15:29:26 +0000

    Completed assessment

--- coding-challenges.js
+++ coding-challenges.js
@@ -4,57 +4,154 @@
 
 
 
+//This is a tricky one. Had to check google a bunch and I was REALLY hoping there was a built in method for shuffle. There isnt, sad day. Found out using a simple math.random() would be a really bad idea for a multitude of reasons. Link to said reasons and why we are going to go a little more the direction of a Fisher-Yates Algo. https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
 
 
+//Creating our function that will shuffle and shift.
+const shuffleShift = ( array ) => {
+    
+//Removes the first zeroeth index element.
+
+    array.shift()
+    let tempArray = array
+    
+//If statement that will check if you are a ding-dong and trying to shift out something that doesnt exist in the array then print a custom message. 
+
+    if ( tempArray.length == [] ) {
+        //Custom message return if the array has nothing in it.
+        return "The array is empty, dawg"
+    //Start our else
+    } else { 
+        
+        //Run our for loop to access the array backwards.
+        
+    for( let i = array.length - 1; i > 0; i--){
+        //mafs
+        let j = Math.floor(Math.random() * i)
+        //declaring a temp array that will hold our first set of mafs.
+        tempArray = array[i]
+        //Declaring that array index is equal to our mafs.
+        array[i] = array[j]
+        //Delcaring our tempArray is the true smart guy here. 
+        array[j] = tempArray
+        }
+    }
+}
+
+
+//Moved the variable array down to see it better
 var collections = ["purple", "blue", "green", "yellow", "pink"]
+shuffleShift(collections)
+console.log(collections)
+shuffleShift(collections)
+console.log(collections)
+shuffleShift(collections)
+console.log(collections)
+shuffleShift(collections)
+console.log(collections)
+shuffleShift(collections)
+console.log(collections)
+
+//Ran a whole bunch so we can see it get to an empty a array.
+// ... and it shows a blank array instead. IDK WHAT IS GOING ON. I CANT MAKE THIS DISPLAY MY RETURN! :( HALP
+
 // Expected output example1 (can be a different order): ["yellow", "blue", "pink", "green"]
 // Expected output example2 (can be a different order): ["blue", "green", "pink"]
 // Expected output example3 (can be a different order): ["pink", "green"]
 
-
 // --------------------2) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
-
-
-
-
 
 var cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
 var cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+//Im pretty sure I might be able to use the Math.pow here, but I'm too fuzzy on it still. Also, I'm actually TERRIBLE at math. Like. Very. Very. Very bad. Thanks homeschooling! :D Going to do it longform
+
+const cubeEm = ( nums ) => {
+// Creation of variable to hold our incomming data
+    let output = 0
+// Adding up the numbers using .map method, then just times it by 3. 
+    nums.map( value => {
+        output += value ** 3
+    })
+    //Returning our output for the magic to happen
+    return output
+}
+
+console.log(cubeEm(cubeAndSum1))
+console.log(cubeEm(cubeAndSum2))
+
+
 
 
 // --------------------3) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
-
-
-
-
 
 var nums1 = [3, 56, 90, -8, 0, 23, 6]
 // Expected output: [-8, 90]
 var nums2 = [109, 5, 9, -59, 8, 24]
 // Expected output: [-59, 109]
 
+//Creation of function that will take an array of numbers and returns an array of the minimum and maximum numbers in that order.
+const minMax = ( nums ) => {
+    //Creation of variables to hold data for later.
+    let minimum = nums[0]
+    let maximum = nums[0]
+    //LETS LOOP! :D
+    for (let i = 0; i < nums.length; i++) {
+        //trying my hand at some ternary stuff. wish me luck. im scared. Hopefully it wont... TURN ARRY ON ME LOL! Do you get it? Its a play on the word ternar..... ok, im sorry. 
+        //First ternary to assign minimum
+        minimum = (minimum > nums[i]) ? nums[i] : minimum
+        //Second ternary to assign maximum. 
+        maximum = (maximum < nums[i]) ? nums[i] : maximum
+    }
+    //Return our minimum and maximum 
+    return [minimum, maximum]
+}
+
+//Console log time to see if it works!
+console.log(minMax(nums1))
+console.log(minMax(nums2))
+// ternary is dope. 
+
+
 
 // --------------------4) Create a function that takes in a string and returns a string with every other letter capitalized.
-
-
-
-
 
 var testString1 = "albatross"
 // Expected output: "aLbAtRoSs"
 var testString2 = "jabberwocky"
 // Expected output: "jAbBeRwOcKy"
 
+//Creation of function that takes in a string and returns a string with every other letter capitalized.
+const upEm = string => {
+    let weirdCaps = ""
+    for (let i = 0; i < string.length; i++){
+        //Ternary that checks the character index. If it TERNS (haha, I did it again) out that the character at the index of the string has a decimal, lets uppercase that badboy using the toUpperCase method. BUT, if it doesnt TERN (hooo boy) out to be that way, lets do nathan about it.
+        weirdCaps += (i%2 == 0) ? string[i]:string[i].toUpperCase()
+    }
+    //Return our final weirdcaps string when called.
+    return weirdCaps
+}
+
+//Calling the function in a console.log.
+console.log(upEm(testString1))
+console.log(upEm(testString2))
+
 
 // --------------------5) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator.
-
-
-
-
 
 var arr1 = [3, 7, 10, 5, 4, 3, 3]
 var arr2 = [7, 8, 2, 3, 1, 5, 4]
 // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+
+//Creration of funciton that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator.
+const dontDupeMeBro = (array1, array2) => {
+    let newArray = new Set( [...array1, ...array2] )
+    //Returning the new array.
+    return [...newArray]
+}
+
+//Calling the function in a console.log
+
+console.log(dontDupeMeBro(arr1,arr2))
--- interview-questions.md
+++ interview-questions.md
@@ -5,37 +5,48 @@ Answer the following questions. First, without external resources. Challenge you
 1. What is an instance variable in Ruby? How is it different from other variables?
 
   Your answer:
-
+  An instance variable uses an @ symbol in the begining and is local to the scope of the current class you are working in.
   Researched answer:
-
+  "In the Ruby programming language, an instance variable is a type of variable which starts with an @ symbol."
+  "An instance variable is used as part of Object-Oriented Programming (OOP) to give objects their own private space to store data."
 
 
 2. What is a block in Ruby?
 
   Your answer:
+  A section containing the logic or data of the individual method. 
 
   Researched answer:
+  "A Ruby block is a way of grouping statements, and may appear only in the source adjacent to a method call"
 
 
 
 3. Ruby has an implicit return. What does that mean?
 
   Your answer:
+  ... a bit fuzzy on this but I feel its the fact we dont have to specifically use return{} in our methods and it will use default values/arguments. Will research more! 
 
   Researched answer:
+  "Any statement in Ruby returns the value of the last evaluated expression. A simple example is the getter method. We call a method and expect some value in return. But as we know, Ruby always returns the last evaluated expression"
 
 
 
 4. What is object-oriented programming? How is it different than functional programming?
 
   Your answer:
+  OOP is a methodology/paradigm in programming. (its a state of mind maaaaan)
+  OOP is different from functional programming in the sense that we do not use variables and functions nor do we build our methods out by themselves. Rather we use objects that will contain not only the methods and values but other data as well. 
 
   Researched answer:
-
-
+  The basic elements of functional programming are Variables and Functions. The basic elements of object-oriented programming are objects and methods. The functional programming is used only when there are few things with more operations. Object-oriented programming is used when there are many things with few operations.
 
 5. What is the difference between a class and an object?
 
   Your answer:
+  An object is simply an entity/instance, without a class it has no meaning. (and it will never find love)
+  A class is the blueprint of an object. It gives the object its structure and logic. (and might still never find love. its code. it has no emotions. cmon now. unless you program that into it. then we're talking about something completely different and.. I digress.)
 
   Researched answer:
+  " A class is a blueprint from which objects are created. The object is also called as an instance of a class. For Example, the animal is a class and mammals, birds, fish, reptiles, and amphibians are the instances of the class."
+  (..still firm on that no love thing.)
+
